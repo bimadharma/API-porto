5b01a24cb348e97fc63eeeba91a6f644df189f38
bimadharma <bimadharmayahya@gmail.com>
2025-02-19 16:41:19 +0700

    update

--- internal/connection/database.go
+++ internal/connection/database.go
@@ -12,16 +12,10 @@ import (
 var DB *gorm.DB
 
 func ConnectDatabase() {
-	host := os.Getenv("DB_HOST")
-	port := os.Getenv("DB_PORT")
-	user := os.Getenv("DB_USER")
-	password := os.Getenv("DB_PASSWORD")
-	dbname := os.Getenv("DB_NAME")
-
-	// Buat connection string secara manual
 	dsn := fmt.Sprintf(
-		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
-		host, port, user, password, dbname,
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=%s",
+		os.Getenv("DB_HOST"), os.Getenv("DB_USER"), os.Getenv("DB_PASS"),
+		os.Getenv("DB_NAME"), os.Getenv("DB_PORT"), os.Getenv("DB_TZ"),
 	)
 
 	var err error
