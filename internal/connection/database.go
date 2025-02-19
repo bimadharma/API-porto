b2c06e37c10f939d6da6a04a9eaaf58019cd1799
bimadharma <bimadharmayahya@gmail.com>
2025-02-19 16:26:30 +0700

    database

--- internal/connection/database.go
+++ internal/connection/database.go
@@ -12,7 +12,17 @@ import (
 var DB *gorm.DB
 
 func ConnectDatabase() {
-	dsn := os.Getenv("DATABASE_URL") // Railway memberikan ini langsung
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+
+	// Buat connection string secara manual
+	dsn := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname,
+	)
 
 	var err error
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
